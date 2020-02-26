8de8f8577c7ca7d194dda97d2348d47543b737fb
Matt Moore <matt.e.moore@gmail.com>
2020-02-25 18:29:21 -0600

    interrupt handling comments + indents

--- helloworld.asm
+++ helloworld.asm
@@ -1,6 +1,11 @@
+PPUCTRL=$2000
+PPUMASK=$2001
+PPUSTATUS=$2002
+PPUADDR=$2006
+PPUDATA=$2007
+
 .segment "HEADER"                         ; iNes header (e.g. 2x16KB PRG-ROMs, 1x8KB CHR-ROM, mapper 0)
 .byte "NES", 26, 2, 1, 0, 0
-
 
 .segment "CODE"                           ; PRG-ROM
 .proc irq_handler                         ; sound/catridge interrupt
@@ -12,22 +17,19 @@
 .endproc
 
 .proc reset_handler                       ; startup/reset button interrupt
-            SEI
+            SEI                           ; ignore interrupts
             CLD
-            LDX #$00
-            STX $2000
-            STX $2001
-vblankwait: BIT $2002
+
+            LDX #$00                      ; disable rendering during startup
+            STX PPUCTRL
+            STX PPUMASK
+
+vblankwait: BIT PPUSTATUS                 ; wait until PPU ready
             BPL vblankwait
             JMP main
 .endproc
 
 .proc main
-                  PPUMASK=$2001
-                  PPUSTATUS=$2002
-                  PPUADDR=$2006
-                  PPUDATA=$2007
-
             LDX   PPUSTATUS               ; prep PPU for writing
 
             LDX   #$3F                    ; store PPU write address ($3F00 is address of first pallette)
