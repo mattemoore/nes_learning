f6ae372635008754b024cafc282374d658880c0a
Matt Moore <matt.e.moore@gmail.com>
2020-02-25 18:18:15 -0600

    indents

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # nes_learning
+
 Basic NES game dev - 6502 assembly
 
-Running example/tutorial from https://book.famicom.party/
+Running example/tutorial from <https://book.famicom.party/>
 Setup on OSX:
+
 1. `brew install cc65`
-2. Copy NES emulator Nintaco binaries https://nintaco.com/ to `<src>\nintaco\bin` 
-3. OPTIONAL: Copy `src` and `api` binaries to `<src>\nintaco\<src or bin>` 
-4. `./run.sh`
+2. Copy NES emulator Nintaco binaries <https://nintaco.com/> to `<src>\nintaco\bin`
+3. OPTIONAL: Copy `src` and `api` binaries to `<src>\nintaco\<src or bin>`
+4. OPTIONAL FOR VSCODE: Install `sharpninja.cc65` vscode extension (cc65 assembler file type)
+5. `./run.sh`
--- helloworld.asm
+++ helloworld.asm
@@ -1,55 +1,51 @@
+; iNes header (e.g. 2x16KB PRG-ROMs, 1x8KB CHR-ROM, mapper 0)
 .segment "HEADER"
 .byte "NES", 26, 2, 1, 0, 0
 
 ; PRG-ROM
 .segment "CODE"
-.proc irq_handler
-  RTI
+.proc irq_handler                         ; sound/catridge interrupt
+            RTI
 .endproc
 
-.proc nmi_handler
-  RTI
+.proc nmi_handler                         ; draw frame interrupt (60/sec)
+            RTI
 .endproc
 
-.proc reset_handler
-  SEI
-  CLD
-  LDX #$00
-  STX $2000
-  STX $2001
-vblankwait:
-  BIT $2002
-  BPL vblankwait
-  JMP main
+.proc reset_handler                       ; startup/reset button interrupt
+            SEI
+            CLD
+            LDX #$00
+            STX $2000
+            STX $2001
+vblankwait: BIT $2002
+            BPL vblankwait
+            JMP main
 .endproc
 
 .proc main
-  PPUMASK=$2001
-  PPUSTATUS=$2002
-  PPUADDR=$2006
-  PPUDATA=$2007
+                  PPUMASK=$2001
+                  PPUSTATUS=$2002
+                  PPUADDR=$2006
+                  PPUDATA=$2007
 
-  ; prep PPU for writing
-  LDX PPUSTATUS
+            LDX   PPUSTATUS               ; prep PPU for writing
 
-  ; store PPU write address ($3F00 is address of first pallette)
-  LDX #$3F
-  STX PPUADDR
-  LDX #$00
-  STX PPUADDR
+            LDX   #$3F                    ; store PPU write address ($3F00 is address of first pallette)
+            STX   PPUADDR
+            LDX   #$00
+            STX   PPUADDR
 
-  ; write color value to write address 
-  LDA #$29
-  STA PPUDATA
+            LDA   #$27                    ; write color value to write address 
+            STA   PPUDATA
 
-  ; draw background
-  LDA #%00011110
-  STA PPUMASK
+            LDA   #%00011110              ; draw background
+            STA   PPUMASK
 
-forever:
-  JMP forever
+forever:    JMP   forever
 .endproc
 
+; specify interrupt handlers
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
 
