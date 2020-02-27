9e054d39ca9289286c5db8b809639498d8fac729
Matt Moore <matt.e.moore@gmail.com>
2020-02-27 12:43:41 -0600

    init basic sprite learning

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -e
 ca65 src/helloworld.asm
 ca65 src/reset.asm
-ld65 *.o -C nes.cfg -o helloworld.nes
+ld65 src/*.o -C nes.cfg -o helloworld.nes
 java --illegal-access=deny -jar tools/nintaco/bin/Nintaco.jar helloworld.nes 
