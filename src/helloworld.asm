c9cb2fcf7723f1967cd4481ba8c8e0a8f84ca9d4
Matt Moore <matt.e.moore@gmail.com>
2020-02-28 15:55:13 -0600

    working sprite render

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -7,10 +7,11 @@
 .endproc
 
 .proc nmi_handler                         ; draw frame interrupt (60/sec)
-            LDA #$00                      ; transfer $0200-$02FF into OAM
+            LDA #$00                      ; transfer a page of OAM buffer ($0200-$02FF) into PPU
             STA OAMADDR
-            LDA #$20
+            LDA #$02
             STA OAMDMA
+            RTI
 .endproc
 
 .import reset_handler                     ; imported reset handler
@@ -19,16 +20,36 @@
 .proc main
             LDX   PPUSTATUS               ; prep PPU for writing
 
+            ; write pallette values in PPU memory
             LDX   #$3F                    ; store PPU write address ($3F00 is address of first pallette)
             STX   PPUADDR
             LDX   #$00
             STX   PPUADDR
-
-            LDA   #$27                    ; write color value to write address 
+            LDA   #$0F
+            STA   PPUDATA                 ; each write increments PPUADDR by one byte
+            LDA   #$09
             STA   PPUDATA
+            LDA   #$19
+            STA   PPUDATA
+            LDA   #$29
 
-            LDA   #%00011110              ; draw background
-            STA   PPUMASK
+            ; copy sprite data to OAM buffer in CPU memory
+            ; NMI interrupt then copies OAM buffer to PPU memory
+            LDA   #$70                    ; Y-coordinate
+            STA   $0200
+            LDA   #$03                    ; tile number
+            STA   $0201
+            LDA   #$00                    ; attributes flag
+            STA   $0202
+            LDA   #$80                    ; X-coordinate
+            STA   $0203
+
+vblankwait: BIT PPUSTATUS
+            BPL vblankwait
+            LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+            STA PPUCTRL
+            LDA #%00011110  ; turn on screen
+            STA PPUMASK
 
 forever:    JMP   forever
 .endproc
@@ -37,6 +58,6 @@ forever:    JMP   forever
 .addr nmi_handler, reset_handler, irq_handler
 
 .segment "CHR"                            ; CHR-ROM
-.res 8192
+.incbin "graphics.chr"
 
 .segment "STARTUP"
