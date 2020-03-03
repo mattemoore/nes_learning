448e18c3588955d7f52e76e65bd1b8a80863b667
Matt Moore <matt.e.moore@gmail.com>
2020-03-03 07:49:25 -0600

    Learn sprites (#1)
    * init basic sprite learning
    
    * init basic sprite learning
    
    * update OAM
    
    * working sprite render
    
    * multiple sprites, proper palette etc.
    
    * multiple sprites, proper palette etc.
    
    * multiple sprites, proper palette etc.

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -7,6 +7,10 @@
 .endproc
 
 .proc nmi_handler                         ; draw frame interrupt (60/sec)
+            LDA #$00                      ; transfer a page of OAM buffer ($0200-$02FF) into PPU
+            STA OAMADDR
+            LDA #$02
+            STA OAMDMA
             RTI
 .endproc
 
@@ -15,17 +19,31 @@
 .export main                              ; make main referrable
 .proc main
             LDX   PPUSTATUS               ; prep PPU for writing
-
-            LDX   #$3F                    ; store PPU write address ($3F00 is address of first pallette)
+            LDX   #$3F                    ; store PPU write address ($3F00 is start of pallette memory)
             STX   PPUADDR
             LDX   #$00
             STX   PPUADDR
-
-            LDA   #$27                    ; write color value to write address 
+          
+            LDX   #$00
+load_plts1: LDA   pallettes,X             ; write background pallette values in PPU memory
             STA   PPUDATA
+            INX
+            CPX   #$20
+            BNE   load_plts1
 
-            LDA   #%00011110              ; draw background
-            STA   PPUMASK
+            LDX   #$00
+load_sprts: LDA   sprites,X               ; write sprite data
+            STA   $0200,X
+            INX
+            CPX   #$10
+            BNE   load_sprts
+
+vblankwait: BIT PPUSTATUS
+            BPL vblankwait
+            LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+            STA PPUCTRL
+            LDA #%00011110  ; render background
+            STA PPUMASK
 
 forever:    JMP   forever
 .endproc
@@ -33,7 +51,25 @@ forever:    JMP   forever
 .segment "VECTORS"                        ; specify interrupt handlers
 .addr nmi_handler, reset_handler, irq_handler
 
-.segment "CHR"                          ; CHR-ROM
-.res 8192
+.segment "CHR"                            ; CHR-ROM
+.incbin "graphics.chr"
+
+.segment "RODATA"                         
+pallettes:  
+; background pallettes
+.byte $11, $11, $11, $11
+.byte $11, $11, $11, $11
+.byte $11, $11, $11, $11
+.byte $11, $11, $11, $11
+; sprite pallettes
+.byte $11, $09, $19, $29
+.byte $11, $03, $21, $31
+.byte $11, $06, $16, $26
+.byte $11, $09, $19, $29
+sprites:
+.byte $07, $03, $00, $00
+.byte $10, $04, $00, $10
+.byte $20, $07, $00, $20
+.byte $30, $07, $00, $30
 
 .segment "STARTUP"
